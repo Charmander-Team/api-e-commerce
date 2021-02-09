6218b69b4890bdc9e779291386c4d51c24ca24b6
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-02-09 14:32:30 +0100

    update index.js for Chris

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const createRoutes = require('./routes');
 // parse application/json
 app.use(bodyParser.json());
 
+// Same origin
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 createRoutes(app);
 
 //Server listening
