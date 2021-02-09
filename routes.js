92c6825e8c5e0c97ac937bca0be8c159ad8a8f38
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-02-09 14:02:36 +0100

    change route path url

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ const handlers = require('./handlers');
 
 const createRoutes = (app) => {
 
-    app.get('/api/products', handlers.showAllCards);
+    app.get('/products', handlers.showAllCards);
 
-    app.get('/api/products/latest', handlers.showLatestCards);
+    app.get('/products/name/:name', handlers.showCardByName);
 
-    app.get('/api/products/:name', handlers.showCardByName);
+    app.get('/products/type/:type', handlers.showCardsFromType);
 
-    app.get('/api/products/type/:type', handlers.showCardsFromType);
+    app.get('/news/products', handlers.showLatestCards);
 
     app.post('/api/products/salameche', handlers.addSalamecheCard);
 
