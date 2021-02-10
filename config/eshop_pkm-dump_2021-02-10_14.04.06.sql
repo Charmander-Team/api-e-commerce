61bf5df884f3df9cb18ca71f388f8882f14457d4
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-02-10 14:05:19 +0100

    :card_file_box: update db

---
+++ config/eshop_pkm-dump_2021-02-10_14.04.06.sql
@@ -0,0 +1,225 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: eshop_pkm
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bid`
+--
+
+DROP TABLE IF EXISTS `bid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bid` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `bid_amount` int(11) DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bid`
+--
+
+LOCK TABLES `bid` WRITE;
+/*!40000 ALTER TABLE `bid` DISABLE KEYS */;
+INSERT INTO `bid` VALUES (1,1,1,5,'open','2021-01-01 23:59:59');
+/*!40000 ALTER TABLE `bid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `delete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'new',NULL,NULL);
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoice`
+--
+
+DROP TABLE IF EXISTS `invoice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `order_ref` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `payment_method` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice`
+--
+
+LOCK TABLES `invoice` WRITE;
+/*!40000 ALTER TABLE `invoice` DISABLE KEYS */;
+INSERT INTO `invoice` VALUES (1,1,'2020-01-01 23:59:59','0001','paypal');
+/*!40000 ALTER TABLE `invoice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `ref` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (1,'0001',1,'in delivery','2020-01-01 23:59:59',1);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `ref` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `energy_type` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `bid` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `delete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'sm76',1,'Pikachu','electric','2021-02-09',0,10,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM76.png',0),(2,'sm226',1,'Dracaufeu','fire','2021-02-09',0,20,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM226.png',0),(3,'sm77',1,'Mewtwo','psychic','2021-02-09',0,30,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM77.png',0),(4,'sm127',1,'Sablaireau d\'alola','steel','2021-02-09',0,40,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM127.png',0),(5,'sm144',1,'Artikodin','water','2021-02-09',0,50,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM144.png',0),(6,'sm153',1,'brindibou','grass','2021-01-01',0,60,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM153.png',0),(7,'sm186',1,'pyroli','fire','2021-01-01',0,70,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM186.png',0),(8,'sm182',1,'persian','normal','2021-01-01',0,20,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM182.png',0),(9,'sm219',1,'entei','fire','2021-01-01',0,120,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM219.png',0),(10,'sm235',1,'évoli','normal','2020-01-01',0,20,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM235.png',0),(11,'sm29',1,'Mimiqui','psychic','2020-01-01',0,25,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM29.png',0),(12,'sm40',1,'bébécaille','dragon','2020-01-01',0,20,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM40.png',0),(13,'sm54',1,'lucario','fighting','2021-01-01',0,10,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SMP/SMP_FR_SM54.png',0),(101,'xy35',2,'poké ball',NULL,'2021-01-01',0,10,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/XY0/XY0_FR_35.png',0),(102,'xy37',2,'potion',NULL,'2021-01-01',0,6,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/XY0/XY0_FR_37.png',0),(103,'xy39',2,'tierno',NULL,'2021-02-09',0,80,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/XY0/XY0_FR_39.png',0),(104,'xy38',2,'echange',NULL,'2017-01-01',0,100,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/XY0/XY0_FR_38.png',0),(105,'sm119',2,'super ball',NULL,'2011-01-01',0,10,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SM1/SM1_FR_119.png',0),(106,'sm129',2,'super bonbon',NULL,'2021-02-09',0,10,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SM1/SM1_FR_129.png',0),(107,'sm133',2,'sbire de la team skull',NULL,'1991-01-01',0,68,'https://assets.pokemon.com/assets/cms2-fr-fr/img/cards/web/SM1/SM1_FR_133.png',0);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock`
+--
+
+LOCK TABLES `stock` WRITE;
+/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+INSERT INTO `stock` VALUES (1,NULL,3);
+/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `firstname` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mail` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'ketchum','sacha','gotacatchemall@hotmail.fr','love_ondine',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'eshop_pkm'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-10 14:04:09
